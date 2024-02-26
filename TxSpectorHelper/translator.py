b6e1076ebb45a649d9567bde49757dde8bcad6bb
jeffchen006 <zhiychen@cs.toronto.edu>
2024-02-26 15:58:11 +0800

    push

--- TxSpectorHelper/translator.py
+++ TxSpectorHelper/translator.py
@@ -120,6 +120,8 @@ class TxSpectorTranslator:
                 value = 0
                 if valueHex != "":
                     value = int(valueHex, 16)
+                if value == 6465080288632887992935785017466142041404191740450231097049595941908663756058008002954484894227235016850239840191280456664183552414061888320434790749718200197151758369117136656398238073201602645534608690999005576107431060412895014698698579951235097826400417372931126123211128042247125007061588114874296665196116381868282817597016550943368878418472137143190076141545767133991730938137977222904073062749399035392185775851860000951790351041733654457243328659296361258953376911461437058545893121962231071280827838980274754543694518738747215045603281448148972411620013120618110485085703611163580344232935300309603613262608762325479780373272565001431976550239562555312795927290179041585775969444647594478272512:
+                    print("now is the time")
                 toAdd += "{}".format(value)
 
             elif opcode == "EXTCODECOPY":
@@ -157,11 +159,11 @@ class TxSpectorTranslator:
                 value = int(valueHex, 16)
                 toAdd += "{}".format(value)
             elif opcode.startswith("DUP"): # DUP1-DUP16
-                pass
+                pass # confirmed
             elif opcode.startswith("SWAP"):
                 pass # confirmed
             elif opcode.startswith("LOG"):
-                pass
+                pass # confirmed
 
             elif opcode == "CREATE":
                 structLog = structLogs[ii]
@@ -177,7 +179,7 @@ class TxSpectorTranslator:
                 print("Reserve create at depth {}: toAdd-{}".format(depth, toAdd))
 
                 self.callReserved[depth] = (toAdd, opcode, None, None)
-
+                continue
 
             elif opcode == "CREATE2":
                 structLog = structLogs[ii]
@@ -193,6 +195,7 @@ class TxSpectorTranslator:
                 print("Reserve create2 at depth {}: toAdd-{}".format(depth, toAdd))
 
                 self.callReserved[depth] = (toAdd, opcode, None, None)
+                continue
 
 
             # Four call opcodes has a special type: 0,1, they need extra type
@@ -310,27 +313,35 @@ class TxSpectorTranslator:
 
 
 
+
+
 def solve1benchmark(exploitTx, use_cache = True):
 
     fe = fetcher()
 
-    path = SCRIPT_DIR + '/../TxSpectorHelper/cache/{}.json.gz'.format(exploitTx)
+    # path = SCRIPT_DIR + '/../TxSpectorHelper/cache2/{}.json'.format(exploitTx)
+    # if not (use_cache and os.path.exists(path)):
+    #     result_dict = fe.getTrace(exploitTx, FullTrace=False)
+    #     with open(path, 'w') as f:
+    #         json.dump(result_dict, f, indent = 2)
+        
 
+    path = SCRIPT_DIR + '/../TxSpectorHelper/cache/{}.json.gz'.format(exploitTx)
     if not (use_cache and os.path.exists(path)):
         result_dict = fe.getTrace(exploitTx, FullTrace=False)
         # with open(path, 'w') as f:
         #     json.dump(result_dict, f, indent = 2)
         writeCompressedJson(path, result_dict)
             
-    # # print(path)
-    # # kk = None
-    # # with open(path, 'r') as f:
-    # #     kk = json.load(f)
-    # kk = readCompressedJson(path)
-    # translated = TxSpectorTranslator().parseLogs(kk)
-    # path = SCRIPT_DIR + '/../TxSpectorHelper/translated/{}.txt'.format(exploitTx)
-    # with open("{}.txt".format(exploitTx), "w") as f:
-    #     f.write(translated)
+    # print(path)
+    # kk = None
+    # with open(path, 'r') as f:
+    #     kk = json.load(f)
+    kk = readCompressedJson(path)
+    translated = TxSpectorTranslator().parseLogs(kk)
+    path = SCRIPT_DIR + '/../TxSpectorHelper/translated/{}.txt'.format(exploitTx)
+    with open("{}.txt".format(exploitTx), "w") as f:
+        f.write(translated)
 
     gc.collect()
 
@@ -344,25 +355,21 @@ if __name__ == "__main__":
     # with open("TxSpectorExample.json", "r") as f:
     #     txspectorExample = json.load(f)
     # # print(txspectorExample)
-    # translated = TxSpectorTranslator().parseLogs(HackTx, txspectorExample)
+    # translated = TxSpectorTranslator().parseLogs(txspectorExample)
     # with open("TxSpectorExampleTranslated.txt", "w") as f:
     #     f.write(translated)
 
+    # Eminence
+    exploitTx = "0x3503253131644dd9f52802d071de74e456570374d586ddd640159cf6fb9b8ad8"
+    solve1benchmark(exploitTx)
 
-   
-    # # Eminence
-    # exploitTx = "0x3503253131644dd9f52802d071de74e456570374d586ddd640159cf6fb9b8ad8"
-    # solve1benchmark(exploitTx)
-   
     # # Harvest1_fUSDT
     # exploitTx = "0x0fc6d2ca064fc841bc9b1c1fad1fbb97bcea5c9a1b2b66ef837f1227e06519a6"
     # solve1benchmark(exploitTx)
 
-
     # # Harvest2_fUSDC
     # exploitTx = "0x35f8d2f572fceaac9288e5d462117850ef2694786992a8c3f6d02612277b0877"
     # solve1benchmark(exploitTx)
-
 
     # # bZx2
     # exploitTx = "0x762881b07feb63c436dee38edd4ff1f7a74c33091e534af56c9f7d49b5ecac15"
@@ -372,23 +379,10 @@ if __name__ == "__main__":
     # exploitTx = '0x8bb8dc5c7c830bac85fa48acad2505e9300a91c3ff239c9517d0cae33b595090'
     # solve1benchmark(exploitTx)
 
-    # # # Warp_interface
-    # # exploitTx = '0x8bb8dc5c7c830bac85fa48acad2505e9300a91c3ff239c9517d0cae33b595090'
-    # # solve1benchmark(exploitTx)
-
-    # # # Warp_interface2
-    # # exploitTx = '0x8bb8dc5c7c830bac85fa48acad2505e9300a91c3ff239c9517d0cae33b595090'
-    # # solve1benchmark(exploitTx)
-    
     # # CheeseBank_1
     # exploitTx = '0x600a869aa3a259158310a233b815ff67ca41eab8961a49918c2031297a02f1cc'
     # solve1benchmark(exploitTx)
-    # # # CheeseBank_2
-    # # exploitTx = '0x600a869aa3a259158310a233b815ff67ca41eab8961a49918c2031297a02f1cc'
-    # # solve1benchmark(exploitTx)
-    # # # CheeseBank_3
-    # # exploitTx = '0x600a869aa3a259158310a233b815ff67ca41eab8961a49918c2031297a02f1cc'
-    # # solve1benchmark(exploitTx)
+
     # # ValueDeFi
     # exploitTx = "0x46a03488247425f845e444b9c10b52ba3c14927c687d38287c0faddc7471150a"
     # solve1benchmark(exploitTx)
@@ -398,25 +392,103 @@ if __name__ == "__main__":
     # # Yearn1
     # exploitTx = '0x59faab5a1911618064f1ffa1e4649d85c99cfd9f0d64dcebbc1af7d7630da98b'
     # solve1benchmark(exploitTx)
-    # # # Yearn1_interface
-    # # exploitTx = '0x59faab5a1911618064f1ffa1e4649d85c99cfd9f0d64dcebbc1af7d7630da98b'
-    # # solve1benchmark(exploitTx)
+
     # # Opyn
     # exploitTx = '0xa858463f30a08c6f3410ed456e59277fbe62ff14225754d2bb0b4f6a75fdc8ad'
     # solve1benchmark(exploitTx)
     # # CreamFi1_1
     # exploitTx = '0x0016745693d68d734faa408b94cdf2d6c95f511b50f47b03909dc599c1dd9ff6'
     # solve1benchmark(exploitTx)
-    # # # CreamFi1_2
-    # # exploitTx = '0x0016745693d68d734faa408b94cdf2d6c95f511b50f47b03909dc599c1dd9ff6'
-    # # solve1benchmark(exploitTx)
+
     # # IndexFi
     # exploitTx = '0x44aad3b853866468161735496a5d9cc961ce5aa872924c5d78673076b1cd95aa'
     # solve1benchmark(exploitTx)
 
-    # CreamFi2_1
-    exploitTx = '0x0fe2542079644e107cbf13690eb9c2c65963ccb79089ff96bfaf8dced2331c92'
-    solve1benchmark(exploitTx)
+    # # CreamFi2_1
+    # exploitTx = '0x0fe2542079644e107cbf13690eb9c2c65963ccb79089ff96bfaf8dced2331c92'
+    # solve1benchmark(exploitTx)
+
+    # # RariCapital1
+    # exploitTx = '0x4764dc6ff19a64fc1b0e57e735661f64d97bc1c44e026317be8765358d0a7392'
+    # solve1benchmark(exploitTx)
+    # # VisorFi
+    # exploitTx = '0x6eabef1bf310a1361041d97897c192581cd9870f6a39040cd24d7de2335b4546'
+    # solve1benchmark(exploitTx)
+    # # UmbrellaNetwork
+    # exploitTx = '0x33479bcfbc792aa0f8103ab0d7a3784788b5b0e1467c81ffbed1b7682660b4fa'
+    # solve1benchmark(exploitTx)
+    # # RevestFi
+    # exploitTx = '0xe0b0c2672b760bef4e2851e91c69c8c0ad135c6987bbf1f43f5846d89e691428'
+    # solve1benchmark(exploitTx)
+
+    # # RoninNetwork
+    # exploitTx = '0xc28fad5e8d5e0ce6a2eaf67b6687be5d58113e16be590824d6cfa1a94467d0b7'
+    # solve1benchmark(exploitTx)
+    # # BeanstalkFarms
+    # exploitTx = '0xcd314668aaa9bbfebaf1a0bd2b6553d01dd58899c508d4729fa7311dc5d33ad7'
+    # solve1benchmark(exploitTx)
+
+    # # HarmonyBridge
+    # exploitTx = '0x27981c7289c372e601c9475e5b5466310be18ed10b59d1ac840145f6e7804c97'
+    # solve1benchmark(exploitTx)
+    # # XCarnival
+    # exploitTx = '0x51cbfd46f21afb44da4fa971f220bd28a14530e1d5da5009cfbdfee012e57e35'
+    # solve1benchmark(exploitTx)
+    # # RariCapital2_1
+    # exploitTx = '0xab486012f21be741c9e674ffda227e30518e8a1e37a5f1d58d0b0d41f6e76530'
+    # solve1benchmark(exploitTx)
+
+    # # DODO
+    # exploitTx = '0x395675b56370a9f5fe8b32badfa80043f5291443bd6c8273900476880fb5221e'
+    # solve1benchmark(exploitTx)
+    # # PickleFi
+    # exploitTx = '0xe72d4e7ba9b5af0cf2a8cfb1e30fd9f388df0ab3da79790be842bfbed11087b0'
+    # solve1benchmark(exploitTx)
+
+    # # Nomad
+    # exploitTx = '0x61497a1a8a8659a06358e130ea590e1eed8956edbd99dbb2048cfb46850a8f17'
+    # solve1benchmark(exploitTx)
+
+    # # PolyNetwork
+    # exploitTx = '0xad7a2c70c958fcd3effbf374d0acf3774a9257577625ae4c838e24b0de17602a'
+    # solve1benchmark(exploitTx)
+
+    # Punk_1
+    # exploitTx = '0x597d11c05563611cb4ad4ed4c57ca53bbe3b7d3fefc37d1ef0724ad58904742b'
+    # solve1benchmark(exploitTx)
+
+
+# ================================================================================================================================================
+
+
+    # # Warp_interface
+    # exploitTx = '0x8bb8dc5c7c830bac85fa48acad2505e9300a91c3ff239c9517d0cae33b595090'
+    # solve1benchmark(exploitTx)
+
+    # # Warp_interface2
+    # exploitTx = '0x8bb8dc5c7c830bac85fa48acad2505e9300a91c3ff239c9517d0cae33b595090'
+    # solve1benchmark(exploitTx)
+
+
+    # # CheeseBank_2
+    # exploitTx = '0x600a869aa3a259158310a233b815ff67ca41eab8961a49918c2031297a02f1cc'
+    # solve1benchmark(exploitTx)
+    # # CheeseBank_3
+    # exploitTx = '0x600a869aa3a259158310a233b815ff67ca41eab8961a49918c2031297a02f1cc'
+    # solve1benchmark(exploitTx)
+
+
+    # # Yearn1_interface
+    # exploitTx = '0x59faab5a1911618064f1ffa1e4649d85c99cfd9f0d64dcebbc1af7d7630da98b'
+    # solve1benchmark(exploitTx)
+
+
+
+    # # CreamFi1_2
+    # exploitTx = '0x0016745693d68d734faa408b94cdf2d6c95f511b50f47b03909dc599c1dd9ff6'
+    # solve1benchmark(exploitTx)
+
+
     # # CreamFi2_2
     # exploitTx = '0x0fe2542079644e107cbf13690eb9c2c65963ccb79089ff96bfaf8dced2331c92'
     # solve1benchmark(exploitTx)
@@ -426,39 +498,17 @@ if __name__ == "__main__":
     # # CreamFi2_4
     # exploitTx = '0x0fe2542079644e107cbf13690eb9c2c65963ccb79089ff96bfaf8dced2331c92'
     # solve1benchmark(exploitTx)
-    # RariCapital1
-    exploitTx = '0x4764dc6ff19a64fc1b0e57e735661f64d97bc1c44e026317be8765358d0a7392'
-    solve1benchmark(exploitTx)
-    # VisorFi
-    exploitTx = '0x6eabef1bf310a1361041d97897c192581cd9870f6a39040cd24d7de2335b4546'
-    solve1benchmark(exploitTx)
-    # UmbrellaNetwork
-    exploitTx = '0x33479bcfbc792aa0f8103ab0d7a3784788b5b0e1467c81ffbed1b7682660b4fa'
-    solve1benchmark(exploitTx)
-    # RevestFi
-    exploitTx = '0xe0b0c2672b760bef4e2851e91c69c8c0ad135c6987bbf1f43f5846d89e691428'
-    solve1benchmark(exploitTx)
+
+
+
     # # RevestFi_interface
     # exploitTx = '0xe0b0c2672b760bef4e2851e91c69c8c0ad135c6987bbf1f43f5846d89e691428'
     # solve1benchmark(exploitTx)
-    # RoninNetwork
-    exploitTx = '0xc28fad5e8d5e0ce6a2eaf67b6687be5d58113e16be590824d6cfa1a94467d0b7'
-    solve1benchmark(exploitTx)
-    # BeanstalkFarms
-    exploitTx = '0xcd314668aaa9bbfebaf1a0bd2b6553d01dd58899c508d4729fa7311dc5d33ad7'
-    solve1benchmark(exploitTx)
+
     # # BeanstalkFarms_interface
     # exploitTx = '0xcd314668aaa9bbfebaf1a0bd2b6553d01dd58899c508d4729fa7311dc5d33ad7'
     # solve1benchmark(exploitTx)
-    # HarmonyBridge
-    exploitTx = '0x27981c7289c372e601c9475e5b5466310be18ed10b59d1ac840145f6e7804c97'
-    solve1benchmark(exploitTx)
-    # XCarnival
-    exploitTx = '0x51cbfd46f21afb44da4fa971f220bd28a14530e1d5da5009cfbdfee012e57e35'
-    solve1benchmark(exploitTx)
-    # RariCapital2_1
-    exploitTx = '0xab486012f21be741c9e674ffda227e30518e8a1e37a5f1d58d0b0d41f6e76530'
-    solve1benchmark(exploitTx)
+
     # # RariCapital2_2
     # exploitTx = '0xab486012f21be741c9e674ffda227e30518e8a1e37a5f1d58d0b0d41f6e76530'
     # solve1benchmark(exploitTx)
@@ -468,25 +518,6 @@ if __name__ == "__main__":
     # # RariCapital2_4
     # exploitTx = '0xab486012f21be741c9e674ffda227e30518e8a1e37a5f1d58d0b0d41f6e76530'
     # solve1benchmark(exploitTx)
-    # DODO
-    exploitTx = '0x395675b56370a9f5fe8b32badfa80043f5291443bd6c8273900476880fb5221e'
-    solve1benchmark(exploitTx)
-    # PickleFi
-    exploitTx = '0xe72d4e7ba9b5af0cf2a8cfb1e30fd9f388df0ab3da79790be842bfbed11087b0'
-    solve1benchmark(exploitTx)
-
-    # Nomad
-    exploitTx = '0x61497a1a8a8659a06358e130ea590e1eed8956edbd99dbb2048cfb46850a8f17'
-    solve1benchmark(exploitTx)
-
-    # PolyNetwork
-    exploitTx = '0xad7a2c70c958fcd3effbf374d0acf3774a9257577625ae4c838e24b0de17602a'
-    solve1benchmark(exploitTx)
-
-    # Punk_1
-    exploitTx = '0x597d11c05563611cb4ad4ed4c57ca53bbe3b7d3fefc37d1ef0724ad58904742b'
-    solve1benchmark(exploitTx)
-
 
     # # Punk_2
     # exploitTx = '0x597d11c05563611cb4ad4ed4c57ca53bbe3b7d3fefc37d1ef0724ad58904742b'
